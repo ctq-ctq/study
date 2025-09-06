7760c212e756837eacb6031406dbe7afba5ff14e
ctq-ctq <liangyetianqi@163.com>
2025-09-06 19:33:34 +0800

    新增学习文件

---
+++ py1.py
@@ -0,0 +1,24 @@
+# # num=-5
+# # print(type(num))
+# num2=1.5
+# print(type(num2)) 
+# print(type(True))
+# print(True+False)
+# print(True+1)
+# print(type(2+3j))
+# name='bingbing'
+# print("我的名字：%s"%name)
+# age=18
+# name='bingbing'
+# print("我的名字：%s,年龄：%d"%(name,age))
+# a=123
+# print('%010d'%a)
+# a=1.2345678
+# print("%f"%a)
+# b=2.34567
+# print("%.5f"%b)
+# print("我是%%的1%%"%())
+# name='bingbing'
+# age=18
+# print(f"我的名字是{name}，我今年{age}岁了")
+print(1+1)
